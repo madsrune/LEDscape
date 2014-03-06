385a9da81fb37f90390f56ab4948e650da67b42d
root <root@beaglebone.(none)>
2014-03-05 17:40:16 -0800

    Cleaned up record/playback code. Added file auto-looping.

--- opc-rx.c
+++ opc-rx.c
@@ -68,8 +68,10 @@ main(
 
 	int fd = 0, fout = 0;
 	int fromfile = FALSE;
+
+	fprintf(stderr, "OpenPixelControl LEDScape Receiver\n\n");
 	
-	while ((opt = getopt(argc, argv, "p:c:d:")) != -1)
+	while ((opt = getopt(argc, argv, "p:c:d:w:r:f:")) != -1)
 	{
 		switch (opt)
 		{
@@ -85,18 +87,19 @@ main(
 			if (sscanf(optarg,"%dx%d", &width, &height) == 2) {
 				led_count = width * height;
 			} else {
-				printf("Invalid argument for -d; expected NxN; actual: %s", optarg);
+				printf("Invalid argument for -d; expected NxN; actual: %s\n", optarg);
 				exit(EXIT_FAILURE);
 			}
 			break;
+			}
 
 		case 'w': 
-			printf("Writing OPC data to file: %s", optarg);
+			printf("Writing OPC data to file: %s\n", optarg);
 			fout = open(optarg, O_WRONLY | O_CREAT | O_TRUNC);
 			break;
 		
 		case 'r': 
-			printf("Reading OPC data from file: %s", optarg);
+			printf("Reading OPC data from file: %s\n", optarg);
 			fd = open(optarg, O_RDONLY);
 			fromfile = TRUE;
 			break;
@@ -105,7 +108,6 @@ main(
 			frame_rate = atoi(optarg);
 			break;
 		
-		}
 		default:
 			fprintf(stderr, "Usage: %s [-p <port>] [-c <led_count> | -d <width>x<height>] [-w <output file>] [-r <input file> [-f <frame rate>]] \n", argv[0]);
 			exit(EXIT_FAILURE);
@@ -113,7 +115,9 @@ main(
 	}
 
 	if (fromfile)
-		printf("Frame rate: %d fps", frame_rate);
+		printf("Frame rate: %d fps\n", frame_rate);
+
+	printf("LEDs per strip: %d\nPort: %d\n", led_count, port);
 
 	const int sock = tcp_socket(port);
 	if (sock < 0)
@@ -125,8 +129,6 @@ main(
 	uint8_t buf[65536];
 	if (sizeof(buf) < image_size + 1)
 		die("%u too large for UDP\n", image_size);
-
-	fprintf(stderr, "OpenPixelControl LEDScape Receiver started on TCP port %d for %d pixels.\n", port, led_count);
 
 	ledscape_t * const leds = ledscape_init(led_count);
 
@@ -152,11 +154,21 @@ main(
 			ssize_t rlen = read(fd, &cmd, sizeof(cmd));
 			
 			if (rlen < 0)
-				die("recv failed: %s\n", strerror(errno));
+					die("recv failed: %s\n", strerror(errno));
 			if (rlen == 0)
 			{
-				close(fd);
-				break;
+				if (fromfile)
+				{
+					printf("looping file\n");
+					lseek(fd, 0, SEEK_SET); // seek to beginning of file
+					rlen = read(fd, &cmd, sizeof(cmd)); // read cmd header again
+				}
+				else
+				{
+					printf("closing socket\n");
+					close(fd);
+					break;
+				}
 			}
 
 			const size_t cmd_len = cmd.len_hi << 8 | cmd.len_lo;
@@ -181,38 +193,25 @@ main(
 			struct timeval start_tv, stop_tv, delta_tv;
 			gettimeofday(&start_tv, NULL);
 
-//			const unsigned frame_num = 0;
-
 			for (unsigned int i=0; i<cmd_len; i++) {
-				//uint8_t * const out = (void*) &frame[i + led_count*cmd.channel];
 				const uint8_t * const in = &buf[3 * i];
-				//out[0] = in[0];
-				//out[1] = in[1];
-				//out[2] = in[2];
-        ledscape_set_color(
-          frame,
-          cmd.channel + i / led_count,
-          i % led_count,
-          
-         in[0],
-         in[1],
-         in[2]
-        );
+			        ledscape_set_color(frame, cmd.channel + i / led_count, i % led_count, 
+							in[0], in[1], in[2]);
  			}
 
 			ledscape_draw(leds, 0);
 
+			gettimeofday(&stop_tv, NULL);
+			timersub(&stop_tv, &start_tv, &delta_tv);
+
 			if (fromfile)
-				usleep(1000000/frame_rate); // Not accurate because we are ignoring processing delay in this loop
+				usleep(1000000/frame_rate-225); // Approximation 
 				
 			if (fout)
 			{
 				write(fout, &cmd, sizeof(cmd));
 				write(fout, buf, sizeof(uint8_t)*cmd_len); 
 			}
-
-			gettimeofday(&stop_tv, NULL);
-			timersub(&stop_tv, &start_tv, &delta_tv);
 
 			frames++;
 			delta_sum += delta_tv.tv_usec;
@@ -221,7 +220,7 @@ main(
 			last_report = stop_tv.tv_sec;
 
 			const unsigned delta_avg = delta_sum / frames;
-			printf("\n%6u usec avg, actual %.2f fps (over %u frames)\n",
+			printf("%u usec avg, actual %.2f fps (over %u frames)\n",
 				delta_avg,
 //				report_interval * 1.0e6 / delta_avg,
 				frames * 1.0 / report_interval,
